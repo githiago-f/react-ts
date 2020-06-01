72b8d2fd74731df93d20cd790941b0ce2195c190
githiago-f <tfarias@protonmail.com>
2020-06-01 13:18:39 -0300

    constructing responsive template

--- src/styles/Defaults/index.tsx
+++ src/styles/Defaults/index.tsx
@@ -1,7 +1,3 @@
 import styled from 'styled-components';
 
-export const Container = styled.section`
-    height: 100vh;
-    width: 100vw;
-`;
-
+export const Container = styled.section``;
--- src/styles/theme/Template.tsx
+++ src/styles/theme/Template.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Base } from 'types/props';
 import { Container } from 'styles/Defaults';
 import { SideBar } from 'styles/SideBar';
+import { Content } from 'styles/Content';
 
 export const Template: React.FC<Base> = (props) => {
 	return(
 		<Container>
-			<SideBar/>
-			{/* there will be my projects */}
+            <div className="row">
+                <SideBar/>
+                <Content>
+                    {props?.children}
+                </Content>
+            </div>
 		</Container>
 	);
 };
