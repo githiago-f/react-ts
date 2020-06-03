c1816b44f5d6507364093a68e28fe294134ecd9e
githiago-f <tfarias@protonmail.com>
2020-06-03 00:02:24 -0300

    removing routes to work in github pages - no routing necessary

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Provider } from 'styles/theme/provider';
-import { Router, Switch, Route } from 'react-router-dom';
 import { Home } from 'pages';
 import { createBrowserHistory } from 'history';
 
@@ -9,13 +8,7 @@ const customHistory = createBrowserHistory();
 export const Routes = () => {
 	return (
 		<Provider>
-			<Router history={customHistory}>
-				<Switch>
-					<Route path="/" exact>
-						<Home/>
-					</Route>
-				</Switch>
-			</Router>
+            <Home/>
 		</Provider>
 	);
 };
